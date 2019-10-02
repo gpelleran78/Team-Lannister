2e06c8c10dbfb696f5e386303bed7fa27c30820e
christina <chrstina.sosa024@gmail.com>
2019-10-01 18:00:34 -0700

    bkground2

---
+++ weather.js
@@ -0,0 +1,19 @@
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+     
+     vid.pause();
+	
+	vidFade();
+});
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+	if (vid.paused) {
+vid.play();
+		pauseButton.innerHTML = "Pause";
+	} else {
+        vid.pause();
+       pauseButton.innerHTML = "Paused";
+	}
+})
